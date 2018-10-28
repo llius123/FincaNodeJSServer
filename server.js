001972a423fa7f7f3668ae96116d4e851367955b
llius123 <llius619@gmail.com>
2018-10-28 17:02:34 +0100

    Recojer datos del body(POST)
    Recojo los datos del post.

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 var express = require('express')
-var app = express()
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-  res.end();
+var app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}))
+app.use(cors());
+
+var config = require('./server_config/config');
+var model = require('./server_models/model');
+
+var connection = config.connection;
+
+//Creo el objeto conexion y el objeto cerrar conexcion
+// var mysq_config = model.connect_disconnect;
+// mysq_config = {
+//     'connect': config.connection.connect(),
+//     'disconnect': config.connection.end()
+// }
+// mysq_config.connect = config.connection.connect();
+// mysq_config.disconnect = config.connection.end();
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+    res.send(req.body);
+    res.end();
 })
 
 app.listen(3000)
---
+++ server_models/model.js
@@ -0,0 +1,7 @@
+var connect_disconnect = {
+    'connect':null,
+    'disconnect':null
+};
+module.exports = {
+    connect_disconnect:connect_disconnect
+}
