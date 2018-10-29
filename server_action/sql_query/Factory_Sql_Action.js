d0dcc8803115c49a098d1f63b9bf96c1fea22525
llius123 <llius619@gmail.com>
2018-10-29 22:49:28 +0100

    Actualizado esquema

--- server_action/sql_query/Factory_Sql_Action.js
+++ server_action/sql_query/Factory_Sql_Action.js
@@ -1,7 +1,6 @@
 var { get } = require('./Factory_Sql_Querys');
 
 module.exports.factory_sql_action = function (action, table, data) {
-    var sql;
     switch (action) {
         case 'get':
             return get(table, data);
